701a7f54d8ba4919acf9f92f7654316a49ec58fc
Daniel Hammerin <danne155@hotmail.com>
2022-07-13 23:25:57 +0200

    Major refactor and base functionality works now.

--- src/App.tsx
+++ src/App.tsx
@@ -45,7 +45,7 @@ const TopBarContainer = styled.div`
 `;
 
 const MenuOption = styled.div`
-  font-size: 28px;
+  font-size: 1.5rem;
   color: #ffd319;
   margin: 0 1rem;
 `;
@@ -53,16 +53,17 @@ const MenuOption = styled.div`
 const TyperContainer = styled.div`
   display: flex;
   flex-direction: column;
-  min-width: 600px;
-  max-width: 1200px;
-  height: fit-content;
+  min-width: 50rem;
+  max-width: 50rem;
+  /* max-height: 6.5rem; */
   margin: 10rem 2rem auto 2rem;
   background: #00000036;
   border-radius: 1rem;
+  overflow: hidden;
 `;
 
 const TypeText = styled.div`
-  font-size: 32px;
+  font-size: 2rem;
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
@@ -76,14 +77,18 @@ const Word = styled.div`
   margin: 0 0.5rem;
 `;
 
-const Letter = styled.div`
-  font-size: 32px;
+const Letter = styled.div<{correct: string}>`
+  font-size: 2rem;
   color: #ff901f;
+  
+  ${p => (p.correct === "y" ? `text-shadow: 0 0 10px ${StyleConstants.yellow}, 0 0 20px #fff, 0 0 30px #e60073, 0 0 40px #e60073, 0 0 50px #e60073, 0 0 60px #e60073, 0 0 70px #e60073;` : null)};
+  /* ${p => (p.correct === "n" ? `text-shadow: 0 0 10px ${StyleConstants.red}, 0 0 20px #fff, 0 0 30px #e60073, 0 0 40px #e60073, 0 0 50px #e60073, 0 0 60px #e60073, 0 0 70px #e60073;` : null)}; */
 `;
 
 const Caret = styled.span`
   transition: left 0.1s ease;
-  /* margin-left: -7.29165px; */
+  /* margin-left: -7.29165px;
+  position: absolute; */
   color: #00eeff;
   animation: blink 1.5s infinite 1s;
 
@@ -111,163 +116,199 @@ const TopBar = () => {
 
 const TyperComponent = ({
   wordList,
-  currentWord,
-  currentWordLetter,
-  typedLetters,
   completedWordsList,
-  extraLetters,
-  setTypedLetters
+  currentWord,
+  currentWordIndex,
+  typedLetters,
 
 } : {
   wordList: string[],
-  currentWord: number,
-  currentWordLetter: number,
-  typedLetters: string[],
   completedWordsList: string[],
-  extraLetters: string[],
-  setTypedLetters: React.Dispatch<React.SetStateAction<string[]>>
+  currentWord: string,
+  currentWordIndex: number,
+  typedLetters: string[],
 }) => {
+  const extraLetters = typedLetters.slice(currentWord.length, typedLetters.length);
 
   return (
     <TyperContainer>
       <TypeText>
-      {wordList.map((word, widx) => {
-        const isCurrentWord = currentWord == widx;
-        const isCompletedWord = completedWordsList[widx] == word;
-        //const extraLetters = typedLetters.slice(word.length + 1, typedLetters.length);
-        return (
-          <>
-            {isCurrentWord ?
-            // Is this word the current word to be typed?
-            <Word key={widx}>
-              {
-                word.split("").map((letter, lidx) => {
-                  const isCurrentLetter = currentWordLetter == lidx;
-                  const isCorrectLetter = word.charAt(lidx) == typedLetters[lidx] && lidx < currentWordLetter;
-                  const isBeforeCurrentLetter = lidx > currentWordLetter;
-                  return (
-                    <>
-                      {isCurrentLetter ?
-                        // If it's the next letter to be typed, show the caret infront of it.
-                        <>
-                          <Caret>|</Caret>
-                          <Letter key={widx+lidx} style={{color: StyleConstants.orange}}>{letter}</Letter>
-                        </>
-                        :
-                        // Otherwise it's any other letter in the word.
-                        <>
-                          {isBeforeCurrentLetter ?
-                            // Is the letter before the current letter?
-                            <Letter key={widx+lidx} style={{color: StyleConstants.orange}}>{letter}</Letter>
+        <>
+          { // Render any completed words
+            completedWordsList.length > 0 ?
+              completedWordsList.map((word, widx) => {
+                return (
+                  <>
+                    <Word key={widx}>
+                      {
+                        word.split("").map((letter, lidx) => {
+                          return (
+                            <Letter correct={"y"} key={widx+lidx} style={{color: StyleConstants.yellow}}>{letter}</Letter>
+                          );
+                        })
+                      }
+                    </Word>
+                  </>
+                );
+              })
+              :
+              null
+          }
+          { // Render current word
+            <Word key={currentWordIndex}>
+              <>
+                {
+                  currentWord.split("").map((letter, lidx) => {
+                    const currentLetter = typedLetters.length === lidx;
+                    const correctLetter = typedLetters[lidx] === currentWord.split("")[lidx];
+                    const lastLetter = currentWord.length === lidx + 1;
+                    const hasTypedAnything = typedLetters.length > 0;
+                    const isLetterBeforeCurrentLetter = lidx > typedLetters.length;
+
+                    return (
+                      <>
+                        {
+                          currentLetter ?
+                            <>
+                              <Caret>|</Caret>
+                              <Letter correct={"n"} key={currentWordIndex+lidx} style={{color: StyleConstants.orange}}>{letter}</Letter>
+                            </>
                             :
                             <>
-                              {isCorrectLetter ?
-                                // Is the letter is typed correctly? 
-                                <Letter key={widx+lidx} style={{color: StyleConstants.yellow}}>{letter}</Letter>
-                                :
-                                <Letter key={widx+lidx} style={{color: StyleConstants.red}}>{letter}</Letter>
+                              {
+                                hasTypedAnything ?
+                                  <>
+                                    {
+                                      isLetterBeforeCurrentLetter ?
+                                        <Letter correct={"u"} key={currentWordIndex+lidx} style={{color: StyleConstants.orange}}>{letter}</Letter>
+                                        :
+                                        <>
+                                          {
+                                            correctLetter ?
+                                              <Letter correct={"y"} key={currentWordIndex+lidx} style={{color: StyleConstants.yellow}}>{letter}</Letter>
+                                              :
+                                              <Letter correct={"n"} key={currentWordIndex+lidx} style={{color: StyleConstants.red}}>{typedLetters[lidx]}</Letter>
+                                          }
+                                          {
+                                            lastLetter && extraLetters.length === 0 ?
+                                              <Caret>|</Caret>
+                                              :
+                                              null
+                                          }
+                                        </>
+                                    }
+                                  </>
+                                  :
+                                  <Letter correct={"n"} key={currentWordIndex+lidx} style={{color: StyleConstants.orange}}>{letter}</Letter>
                               }
                             </>
-                          }
-                        </>
-                      }
-                    </>
-                  );
-                })
-              }
-              {extraLetters.map((letter, elidx) => {
-                return (
-                  <Letter key={elidx} style={{color: StyleConstants.red_extra_letter}}>{letter}</Letter>
-                );
-              })}
-            </Word>
-            :
-            // If this isn't the currently typed word, show it without caret logic.
-            <Word key={widx}>
-                {
-                  Array.from(word).map((letter, lidx) => {
-                    return (
-                      <Letter key={widx+lidx} style={{color: isCompletedWord ? StyleConstants.yellow : StyleConstants.orange}}>{letter}</Letter>
+                        }
+                      </>
                     );
                   })
                 }
-              </Word>
-            }
-          </>
-        );
-      })}
+                {
+                  extraLetters ?
+                    extraLetters.map((letter, lidx) => {
+                      const lastLetter = extraLetters.length - 1 === lidx;
+                      return (
+                        <>
+                          <Letter correct={"n"} key={currentWordIndex + lidx} style={{ color: StyleConstants.red_extra_letter }}>{letter}</Letter>
+                          {
+                            lastLetter ?
+                            <Caret>|</Caret>
+                            :
+                            null
+                          }
+                        </>
+                      );
+                    })
+                    :
+                    null
+                }
+              </>
+            </Word>
+          }
+          { // Render any yet to be written words
+            wordList.map((word, widx) => {
+              return (
+                <>
+                  {
+                    widx === currentWordIndex ?
+                      null
+                      :
+                      <Word key={widx}>
+                        {
+                          word.split("").map((letter, lidx) => {
+                            return (
+                              <>
+                                <Letter correct={"u"} key={widx+lidx} style={{color: StyleConstants.orange}}>{letter}</Letter>
+                              </>
+                            );
+                          })
+                        }
+                      </Word>
+                  }
+                </>
+              );
+            })
+          }
+        </>
       </TypeText>
     </TyperContainer>
   );
 }
 
 const App = () => {
-  const [currentWord, setCurrentWord] = useState<number>(0);
-  const [currentWordLetter, setCurrentWordLetter] = useState<number>(0);
-  const [wordList, setWordlist] = useState<string[]>([]);
+  const [wordList, setWordlist] = useState<string[]>(wordsJson);
   const [completedWordsList, setCompletedWordsList] = useState<string[]>([]);
+  const [currentWord, setCurrentWord] = useState<string>("");
+  const [currentWordIndex, setCurrentWordIndex] = useState<number>(0);
   const [typedLetters, setTypedLetters] = useState<string[]>([]);
-  const [extraLetters, setExtraLetters] = useState<string[]>([]);
 
   useEffect(() => {
-    const words: string[] = wordsJson;
-    setWordlist(words);
-  }, [wordList]);
+    if (!currentWord) setCurrentWord(wordList[currentWordIndex]);
+  }, [currentWord, wordList, currentWordIndex]);
 
   useEffect(() => {
 		document.onkeydown = (e) => {
-      if (e.key == "Escape") {
-        console.log(`Typed letters: ${typedLetters}`);
-        console.log(`Current word index: ${currentWord}`);
-        console.log(`Current word letter index: ${currentWordLetter}`);
-        console.log(`Completed words: ${completedWordsList}`);
-        console.log(`Extra letters: ${extraLetters}`);
-        return;
-      }
-
       if (["Shift", "Control", "Alt"].includes(e.key)) {
         return;
       }
 
-      if (e.key == " ") {
-        const activeWord = wordList[currentWord];
+      switch (e.key) {
+        case "Escape":
+          console.log(`Remaining words: ${wordList.length}`);
+          console.log(wordList)
+          console.log(`Completed words: ${completedWordsList.length}`);
+          console.log(completedWordsList);
+          console.log(`Current word: ${currentWord}`);
+          console.log(`Current word index: ${currentWordIndex}`);
+          console.log(`Typed letters: ${typedLetters}`);
+          break;
+        case " ":
+          if (typedLetters.join("") === currentWord) {
+            setCompletedWordsList([...completedWordsList, wordList.splice(0, 1).toString()]);
+            setWordlist(wordList.slice(0, wordList.length));
+            setCurrentWord(wordList[currentWordIndex]);
+            setCurrentWordIndex(currentWordIndex);
+            setTypedLetters([]);
+          }
+          break;
+        case "Backspace":
+          if (typedLetters.length > 0) {
+            setTypedLetters(typedLetters.slice(0, typedLetters.length - 1));
+          }
+          break;
+        default:
+          setTypedLetters([...typedLetters, e.key]);
 
-        if (typedLetters.join("") == activeWord) {
-          console.log(`"${activeWord}" typed correctly.`)
-          setCompletedWordsList(completedWordsList => [...completedWordsList, activeWord]);
-          setCurrentWord(currentWord + 1);
-          setCurrentWordLetter(0);
-          setTypedLetters([]);
-          setExtraLetters([]);
-          return;
-        }
-        console.log("not correct")
-        return;
       }
-
-      if (e.key == "Backspace") {
-        if (typedLetters.length == 0 || currentWordLetter == 0) return;
-
-        if (currentWordLetter == typedLetters.length) {
-          setCurrentWordLetter(currentWordLetter - 1);
-        }
-        setTypedLetters(typedLetters => typedLetters.splice(-1));
-        const extras = typedLetters.slice(wordList[currentWord].length, typedLetters.length);
-        setExtraLetters(extraLetters => [...extraLetters, ...extras]);
-        return;
-      }
-
-      if (currentWordLetter < wordList[currentWord].length) {
-        setCurrentWordLetter(currentWordLetter + 1);
-      }
-      setTypedLetters(typedLetters => [...typedLetters, e.key]);
-      const extras = typedLetters.slice(wordList[currentWord].length, typedLetters.length);
-      setExtraLetters(extras);
 		};
 		return () => {
 			document.onkeydown = null;
 		};
-	}, [typedLetters, currentWordLetter, currentWord, extraLetters, wordList]);
+	}, [typedLetters, currentWord, wordList, currentWordIndex, completedWordsList]);
 
   return (
     <AppContainer style={{backgroundImage: `url(${Background})`}}>
@@ -275,12 +316,10 @@ const App = () => {
         <TopBar />
         <TyperComponent 
           wordList = {wordList} 
-          currentWord = {currentWord} 
-          currentWordLetter = {currentWordLetter}
-          typedLetters = {typedLetters}
           completedWordsList = {completedWordsList}
-          extraLetters = {extraLetters}
-          setTypedLetters = {setTypedLetters}
+          currentWord = {currentWord} 
+          currentWordIndex = {currentWordIndex}
+          typedLetters = {typedLetters}
           />
       </Content>
     </AppContainer>
