5a640cb97525012320bdfda94334a48ee83a1e62
Daniel Hammerin <danne155@hotmail.com>
2022-07-24 00:21:19 +0200

    Added font awesome for some icons. Implemented a timer and wpm counter. Added tiemr options. Added reset functionality to reset test.

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import Background from "../src/assets/images/wallpapersden.com_sunset-retrowave-
 import './App.css';
 import styled from "styled-components";
 import wordsJson from "./config/wordlists/words.json";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRotateLeft } from "@fortawesome/free-solid-svg-icons";
 
 const StyleConstants = {
   yellow: "#ffd319",
@@ -29,6 +31,19 @@ const AppContainer = styled.div`
   background-position: center;
 `;
 
+const Title = styled.div`
+  display: flex;
+  height: fit-content;
+  width: fit-content;
+  align-items: center;
+  justify-content: center;
+  margin: 2rem;
+
+  font-size: 3rem;
+  color: ${StyleConstants.yellow};
+  text-shadow: 0 0 10px ${StyleConstants.yellow}, 0 0 20px #fff, 0 0 30px #e60073, 0 0 40px #e60073, 0 0 50px #e60073, 0 0 60px #e60073, 0 0 70px #e60073;
+`;
+
 const Content = styled.div`
   display: flex;
   flex-direction: column;
@@ -41,7 +56,7 @@ const Content = styled.div`
 const TopBarContainer = styled.div`
   display: flex;
   flex-direction: row;
-  margin: 2rem auto;
+  margin: 0 auto;
 `;
 
 const MenuOption = styled.div`
@@ -50,22 +65,49 @@ const MenuOption = styled.div`
   margin: 0 1rem;
 `;
 
+const TimerBar = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 52rem;
+  height: fit-content;
+  margin: 5rem 2rem 0rem 2rem;
+`;
+
 const TyperContainer = styled.div`
   display: flex;
   flex-direction: column;
   min-width: 50rem;
   max-width: 50rem;
   max-height: 9rem;
-  margin: 10rem 2rem auto 2rem;
+  margin: 0.5rem 2rem auto 2rem;
   padding: 1rem;
   background: #00000036;
   border-radius: 1rem;
 `;
 
+const ResultsScreen = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  min-width: 50rem;
+  max-width: 50rem;
+  height: 9rem;
+  margin: 0.5rem 2rem auto 2rem;
+  padding: 2rem 1rem;
+  background: #00000036;
+  border-radius: 1rem;
+
+  font-size: 1.5rem;
+  color: ${StyleConstants.yellow};
+  text-shadow: 0 0 10px ${StyleConstants.yellow}, 0 0 10px #fff, 0 0 10px #e60073, 0 0 15px #e60073;
+`;
+
 const TypeText = styled.div`
   display: flex;
   justify-content: center;
-  height: 7rem;
+  height: 7.1rem;
   overflow: hidden;
 `;
 
@@ -76,6 +118,7 @@ const TypeTextContent = styled.div<{offset: number}>`
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
+  transition: all 150ms;
 `;
 
 const Word = styled.div`
@@ -93,6 +136,37 @@ const Letter = styled.div<{correct: string}>`
   ${p => (p.correct === "y" ? `text-shadow: 0 0 10px ${StyleConstants.yellow}, 0 0 10px #fff, 0 0 10px #e60073, 0 0 15px #e60073` : null)};
   /* ${p => (p.correct === "y" ? `text-shadow: 0 0 10px ${StyleConstants.yellow}, 0 0 20px #fff, 0 0 30px #e60073, 0 0 40px #e60073, 0 0 50px #e60073, 0 0 60px #e60073, 0 0 70px #e60073;` : null)}; */
   /* ${p => (p.correct === "n" ? `text-shadow: 0 0 10px ${StyleConstants.red}, 0 0 20px #fff, 0 0 30px #e60073, 0 0 40px #e60073, 0 0 50px #e60073, 0 0 60px #e60073, 0 0 70px #e60073;` : null)}; */
+`;
+
+const Timer = styled.div`
+  display: flex;
+  font-size: 1.5rem;
+  color: ${StyleConstants.yellow};
+  text-shadow: 0 0 10px ${StyleConstants.yellow}, 0 0 10px #fff, 0 0 10px #e60073, 0 0 15px #e60073;
+  background: #00000036;
+  border-radius: 1rem;
+  height: 1.5rem;
+  width: fit-content;
+  padding: 1rem;
+`;
+
+const TimeSelector = styled.div`
+  display: flex;
+  font-size: 1.5rem;
+  color: #ff901f;
+  background: #00000036;
+  border-radius: 1rem;
+  height: 1.5rem;
+  width: fit-content;
+  padding: 1rem;
+`;
+
+const TimerOption = styled.div<{selected: boolean}>`
+  display: flex;
+  font-size: 1.5rem;
+  margin: 0 0.5rem;
+  cursor: pointer;
+  ${p => (p.selected ? `color: ${StyleConstants.yellow}; text-shadow: 0 0 10px ${StyleConstants.yellow}, 0 0 10px #fff, 0 0 10px #e60073, 0 0 15px #e60073` : null)};
 `;
 
 const Caret = styled.span<{offset: number}>`
@@ -121,6 +195,48 @@ const TopBar = () => {
       <MenuOption>Hello</MenuOption>
       <MenuOption>Hello</MenuOption>
     </TopBarContainer>
+  );
+}
+
+const TimerComponent = ({
+  timeRemaining, 
+  setTestDuration,
+  reset
+} : {
+  timeRemaining: number, 
+  setTestDuration: React.Dispatch<React.SetStateAction<number>>,
+  reset: Function
+}) => {
+  const [selected, setselected] = useState<number>(30);
+  const timerTimes = [30, 60, 120];
+
+  const TimerOptionComponent = ({time} : {time: number}) => {
+    return (
+      <TimerOption 
+        selected={selected === time} 
+        onClick={() => {
+          setselected(time); 
+          setTestDuration(time);
+          reset();
+        }}>
+          {time}
+        </TimerOption>
+    );
+  }
+
+  return (
+    <TimerBar>
+      <Timer>{timeRemaining}</Timer>
+      <TimeSelector>
+        {
+          timerTimes.map((time, idx) => {
+            return (
+              <TimerOptionComponent time={time} key={`timer_option_${idx}`}/>
+            );
+          })
+        }
+      </TimeSelector>
+    </TimerBar>
   );
 }
 
@@ -324,6 +440,23 @@ const TyperComponent = ({
   );
 }
 
+const ResultsComponent = ({
+  completedWords,
+  testDuration,
+  reset
+} : {
+  completedWords: number,
+  testDuration: number,
+  reset: Function
+}) => {
+  return (
+    <ResultsScreen>
+      {(completedWords / testDuration) * 60} WPM
+      <FontAwesomeIcon icon={faRotateLeft} onClick={() => {reset();}} style={{cursor: "pointer"}}/>
+    </ResultsScreen>
+  );
+}
+
 const App = () => {
   const [wordList, setWordlist] = useState<string[]>(wordsJson);
   const [completedWordsList, setCompletedWordsList] = useState<string[]>([]);
@@ -332,6 +465,10 @@ const App = () => {
   const [typedLetters, setTypedLetters] = useState<string[]>([]);
   const [textVerticalOffset, setTextVerticalOffset] = useState<number>(0);
 
+  const [testInProgress, setTestInProgress] = useState<boolean>(false);
+  const [testDuration, setTestDuration] = useState<number>(30);
+  const [timeRemaining, setTimeRemaining] = useState<number>(30);
+
   const shuffleWordList = (list: string[]) => {
     for (let i = list.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
@@ -339,20 +476,46 @@ const App = () => {
       list[i] = list[j];
       list[j] = temp;
     }
+    return list;
   };
 
   const shouldOffset = (offset: number) => {
     const currentWord = document.getElementById("current_word");
     if (currentWord) {
-      console.log(currentWord.offsetTop);
       if (currentWord.offsetTop <= 38) return false;
       return (currentWord.offsetTop > (38 * offset));
     }
-  }
+  };
+
+  const reset = () => {
+    setWordlist(shuffleWordList(wordsJson));
+    setCompletedWordsList([]);
+    setCurrentWord("");
+    setCurrentWordIndex(0);
+    setTypedLetters([]);
+    setTextVerticalOffset(0);
+    setTestInProgress(false);
+    setTimeRemaining(testDuration);
+  };
 
   useEffect(() => {
     shuffleWordList(wordsJson);
   }, []);
+
+  useEffect(() => {
+    setTimeRemaining(testDuration)
+  }, [testDuration]);
+
+  useEffect(() => {
+    if (!testInProgress) return;
+    if (timeRemaining === 0) return setTestInProgress(false);
+
+    const interval = setInterval(() => {
+      setTimeRemaining(timeRemaining - 1);
+    }, 1000);
+    
+    return () => clearInterval(interval);
+  }, [timeRemaining, testInProgress]);
 
   useEffect(() => {
     if (!currentWord) setCurrentWord(wordList[currentWordIndex]);
@@ -360,9 +523,12 @@ const App = () => {
 
   useEffect(() => {
 		document.onkeydown = (e) => {
+      e.preventDefault();
       if (["Shift", "Control", "Alt"].includes(e.key)) {
         return;
       }
+
+      if (!testInProgress) setTestInProgress(true);
 
       switch (e.key) {
         case "Escape":
@@ -388,11 +554,13 @@ const App = () => {
             setTypedLetters(typedLetters.slice(0, typedLetters.length - 1));
           }
           break;
-        case "q":
-          setTextVerticalOffset(textVerticalOffset + 1);
+        case "Tab":
+          reset();
           break;
         default:
+          if (!testInProgress) setTestInProgress(true);
           setTypedLetters([...typedLetters, e.key]);
+
           if (shouldOffset(textVerticalOffset + 1)) {
             setTextVerticalOffset(textVerticalOffset + 1);
           }
@@ -401,20 +569,39 @@ const App = () => {
 		return () => {
 			document.onkeydown = null;
 		};
-	}, [typedLetters, currentWord, wordList, currentWordIndex, completedWordsList, textVerticalOffset]);
+	}, [
+    typedLetters, 
+    currentWord, 
+    wordList, 
+    currentWordIndex, 
+    completedWordsList, 
+    textVerticalOffset, 
+    testInProgress
+  ]);
 
   return (
     <AppContainer style={{backgroundImage: `url(${Background})`}}>
       <Content>
+        <Title>Pineapple Typer</Title>
         <TopBar />
-        <TyperComponent 
-          wordList = {wordList} 
-          completedWordsList = {completedWordsList}
-          currentWord = {currentWord} 
-          currentWordIndex = {currentWordIndex}
-          typedLetters = {typedLetters}
-          textVerticalOffset = {textVerticalOffset}
-          />
+        <TimerComponent timeRemaining = {timeRemaining} setTestDuration = {setTestDuration} reset = {reset}/>
+        {
+          timeRemaining === 0 ?
+            <ResultsComponent 
+              completedWords = {completedWordsList.length} 
+              testDuration = {testDuration}
+              reset = {reset}
+            />
+            :
+            <TyperComponent 
+              wordList = {wordList} 
+              completedWordsList = {completedWordsList}
+              currentWord = {currentWord} 
+              currentWordIndex = {currentWordIndex}
+              typedLetters = {typedLetters}
+              textVerticalOffset = {textVerticalOffset}
+            />
+        }
       </Content>
     </AppContainer>
   );
